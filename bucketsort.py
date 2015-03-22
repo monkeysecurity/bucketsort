e1d5d81ad8531695f6a768eeafceaf8f15b31bfd
Patrick Kelley <pkelley@netflix.com>
2015-03-22 16:13:12 -0700

    Adding a recursive bucket sort

--- bucketsort.py
+++ bucketsort.py
@@ -4,6 +4,7 @@ Bucket sort.
 Usage:
   bucketsort generate_phone_numbers [--number=<number>] --file=<filename>
   bucketsort sort_numbers --file=<filename> [--num_buckets=<num_buckets>]
+  bucketsort sort_numbers --recursive --file=<filename>
 
 Options:
   -h --help     Show this screen.
@@ -11,6 +12,7 @@ Options:
   --number=<number>   Number of phone numbers to generate. [Default: 10].
   --file=<filename>   Filename to read/write json to. [Default: output.json].
   --num_buckets=<num_buckets>  Number of buckets to use in sorting [Default: 10].
+  --recursive        If set, bucketsort will not call insertion sort when n < 10.
 """
 from docopt import docopt
 from random import randint
@@ -56,6 +58,14 @@ def _insertion_sort(seq):
 
 
 def bucket_sort(input, num_buckets=10):
+    """
+    Bucket sort with configurable number of buckets.
+    Each bucket is run through insertion sort
+
+    :param input: list of phone numbers to sort
+    :param num_buckets: The number of buckets to use.
+    :return: sorted list of phone numbers
+    """
     # 1) make buckets
     buckets = {i:[] for i in range(0,int(num_buckets))}
 
@@ -78,11 +88,45 @@ def bucket_sort(input, num_buckets=10):
 
     # 4) sort individual buckets with insertion sort
     for _,bucket in buckets.items():
-        bucket = _insertion_sort(bucket)
+        _insertion_sort(bucket)
 
     # 5) concat all buckets into output string
     sorted_phone_numbers = []
     for i in range(0,int(num_buckets)):
+        sorted_phone_numbers.extend(buckets[i])
+    return sorted_phone_numbers
+
+
+def bucket_sort_recursive(input, character=0):
+    """
+    Recursive Bucket sort with 10 preset buckets.
+    Each bucket is run through bucket sort again, but
+    concentrating on a different character in the phone number.
+
+    :param input: list of phone numbers to sort
+    :param character: The digit in the phone number to work on.
+    :return: sorted list of phone numbers
+    """
+
+    if len(input) < 10:
+        _insertion_sort(input)
+        return input
+
+    # 1) make buckets
+    buckets = {i:[] for i in range(0,10)}
+
+    # 3) insert items into buckets
+    for item in input:
+        buckets[int(item[character])].append(item)
+
+    # 4) sort individual buckets recursively
+    if character < 8:
+        for i in range(0,10):
+            buckets[i] = bucket_sort_recursive(buckets[i], character=character+1)
+
+    # 5) concat all buckets into output string
+    sorted_phone_numbers = []
+    for i in range(0,10):
         sorted_phone_numbers.extend(buckets[i])
     return sorted_phone_numbers
 
@@ -101,8 +145,14 @@ if __name__ == "__main__":
 
     elif arguments['sort_numbers']:
         input = read_file(arguments['--file'])
-        sorted_numbers = bucket_sort(input, num_buckets=arguments['--num_buckets'])
-        # print json.dumps(sorted_numbers, indent=2)
+        if arguments['--recursive']:
+            sorted_numbers = bucket_sort_recursive(input, character=0)
+        else:
+            # This one performs best.
+            sorted_numbers = bucket_sort(input, num_buckets=arguments['--num_buckets'])
+            
+        # Probably want to comment this out for speed comparisons:
+        print json.dumps(sorted_numbers, indent=2)
 
     time2 = time.time()
     print "Took %0.1f s" % (time2-time1)
